bcf739af89c718ba3ac78aa0df0b94ffdd4a3d05
achungRapidSOS <achung@rapidsos.com>
2024-07-10 11:44:10 -0400

    Added session notes

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+const DAYS = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"]
 function daysInMonth(year, month) {
      return new Date(year, month-1, 0).getDate();
 }
@@ -45,12 +46,9 @@ function DaysOfMonth({currMonth, currYear}){
     console.log(`Curr month ${currMonth} curr year ${currYear}`)
 
     let numOfDays = daysInMonth(currYear, currMonth);
-    console.log(`Num of days ${numOfDays}`)
     let firstDay = new Date(currYear, currMonth, 1).getDay()
-    console.log(`First day of month ${firstDay}`)
     let lastDatePrevMonth = new Date(currYear, currMonth-1, 0).getDate() - 1
 
-    console.log(lastDatePrevMonth)
     let rows = []
     let row =[]
 
@@ -72,48 +70,74 @@ function DaysOfMonth({currMonth, currYear}){
     }
 
     rows.push(<tr>{currRow}</tr>);
-    
+
 
     return rows
 }
 
-function DaysOfWeekRow(){
+function DaysOfWeekRow() {
     let column_days = []
     for (const day of DAYS) {
         column_days.push(
-            <th>{day}</th>
+            <th key={day}>{day}</th>
         )
     }
 
     return (
         <tr>{column_days}</tr>
     )
+}
+function JournalLogNotes() {
+    return (
+        <div>
+            <p>Session Notes</p>
+            <form>
+                <input type="text"></input>
+            </form>
+        </div>
+
+    )
 
 }
 
-// Parent component containing search bar and product table
-function GymJournalBody({ products }) {
+function JournalGradeNotes() {
     return (
         <div>
+            <p>Highest grade </p>
+            <form>
+                <input type="text"></input>
+            </form>
+
+            <p>Grade total </p>
+            <form>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+
+function JournalLog() {
+    return (
+        <div>
+            <JournalGradeNotes/>
+            <JournalLogNotes/>
+        </div>
+    )
+}
+
+
+// Parent component containing search bar and product table
+function ClimbingJournalBody() {
+    return (
+        <div>
+            <h1>My Climbing Journal</h1>
             <Calender />
+            <JournalLog />
         </div>
     );
 }
 
-const PRODUCTS = [
-    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
 
-const MONTHS = ["January", "February", "March", "April", "May", "June"
-, "July", "August", "September", "October", "November", "December"
-]
-
-const DAYS = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"]
 export default function App() {
-    return <GymJournalBody products={PRODUCTS} />;
+    return <ClimbingJournalBody />;
 }
