c4f4f9b82e79919dfe70777262a3a2af1722e285
alexchung1233 <awchung@email.wm.edu>
2024-10-08 14:33:11 -0400

    Updated site footer

--- app/src/components/SiteFooter.js
+++ app/src/components/SiteFooter.js
@@ -7,7 +7,7 @@ export function SiteFooter() {
     <Footer container>
       <Footer.Copyright href="#" by="My Climbing Journal™" year={2024} />
       <Footer.LinkGroup>
-        <Footer.Link>Alex Chung</Footer.Link>
+        <Footer.Link>Created by Alex Chung</Footer.Link>
         <Footer.Link>awchung1357@gmail.com</Footer.Link>
         <Footer.Link href="#">About</Footer.Link>
      </Footer.LinkGroup>
