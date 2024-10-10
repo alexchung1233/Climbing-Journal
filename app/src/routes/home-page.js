789dd7010fcbcacfb1fa03e74fc13078a3bad4ac
alexchung1233 <awchung@email.wm.edu>
2024-10-10 16:30:17 -0400

    Added home page photo

--- app/src/routes/home-page.js
+++ app/src/routes/home-page.js
@@ -11,6 +11,12 @@ return (
           </ul>
         </nav>
       </div>
+      <div class="flex flex-row min-h-screen justify-center items-center">
+        <h2>
+          What are you going to send today?.
+        </h2>
+          <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/man-rock-climbing-indoors-rear-view-joe-mcbride.jpg"></img>
+      </div>
       <div id="detail"></div>
     </>
     );
