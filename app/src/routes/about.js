bf425d59c5f75b4fcdaa644d6cf3af9ce5192f14
alexchung1233 <awchung@email.wm.edu>
2024-10-14 23:05:43 -0400

    Updated about

--- app/src/routes/about.js
+++ app/src/routes/about.js
@@ -4,6 +4,8 @@ export default function AboutPage(){
         <p className="container">
             Hi. I created this as a personal journal site for my rock climbing training, but I want others to use it as much as I do. Feel free to send me an email for 
             any suggestions.
+
+            No ads. No pop ups. Just a simple training journal based on a real climber's needs.
         </p>
     </div>)    
 }
